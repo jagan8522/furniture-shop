38c2e9c68ffeeb1bd211998047a2880931a8d006
jagan8522 <jagankurra89@gmail.com>
2025-10-03 16:29:21 +0530

    update 1

--- story/script.js
+++ story/script.js
@@ -1,17 +1,17 @@
-const openSearch = document.getElementById('openSearch');
-const closeSearch = document.getElementById('closeSearch');
-const searchOverlay = document.getElementById('searchOverlay');
+// const openSearch = document.getElementById('openSearch');
+// const closeSearch = document.getElementById('closeSearch');
+// const searchOverlay = document.getElementById('searchOverlay');
 
-// Show overlay
-if (openSearch) {
-  openSearch.addEventListener('click', () => {
-    searchOverlay.classList.add('show');
-  });
-}
+// // Show overlay
+// if (openSearch) {
+//   openSearch.addEventListener('click', () => {
+//     searchOverlay.classList.add('show');
+//   });
+// }
 
-// Hide overlay
-if (closeSearch) {
-  closeSearch.addEventListener('click', () => {
-    searchOverlay.classList.remove('show');
-  });
-}
+// // Hide overlay
+// if (closeSearch) {
+//   closeSearch.addEventListener('click', () => {
+//     searchOverlay.classList.remove('show');
+//   });
+// }
